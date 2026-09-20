c499da1e198c6bfcec2747e25df7d394a46102d2
s-karmakar <rohankarmakar3d@gmail.com>
2026-09-20 16:02:19 +0530

    Done till Routing using React Router

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Body from "./Body";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./Login";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
