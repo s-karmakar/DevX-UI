58495ae127af075a96f18577b716bba8d6f49f0f
s-karmakar <rohankarmakar3d@gmail.com>
2026-09-22 18:39:35 +0530

    just moved components to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Body from "./Body";
+import Body from "./components/Body";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Login from "./Login";
-import Profile from "./Profile";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 import appStore from "./utils/appStore";
 import { Provider } from "react-redux";
-import Feed from "./Feed";
+import Feed from "./components/Feed";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
-              <Route path ="/" element={<Feed/>} /> 
+              <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
             </Route>
